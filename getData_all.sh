9ca723f38747f9128ba6f8c5b74c6daaae572f88
jingqiliu <lasiwk697@outlook.com>
2025-06-22 21:03:19 -0400

    Update getData_all.sh

--- getData_all.sh
+++ getData_all.sh
@@ -10,8 +10,9 @@ scp /User/Jingqi/Desktop/Project/Bergland_Lab/In(2L)t_Tracking/getData_all.R cqh
 #SBATCH --account=berglandlab
 #SBATCH --output=/scratch/cqh6wn/In2Lt_Tracking/getdata_all.%A_%a.out
 #SBATCH --error=/scratch/cqh6wn/In2Lt_Tracking/getdata_all.%A_%a.err
-#SBATCH --array=1-500 /scratch/cqh6wn/In2Lt_Tracking/getData_all.sh
+#SBATCH --array=1-500
 
 module load gcc/11.4.0 openmpi/4.1.4 R/4.3.1; echo "R_LIBS_USER=~/R/goolf/4.3" > ~/.Renviron
 
 Rscript /scratch/cqh6wn/In2Lt_Tracking/getData_all.R $SLURM_ARRAY_TASK_ID
+
